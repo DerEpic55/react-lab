8a57eefdcf036de717f48e2e46daa944a3a028f1
DerEpic55 <dima20021234@gmail.com>
2026-01-09 23:33:45 +0600

    1.5.1

--- src/1/1_5_1/App.tsx
+++ src/1/1_5_1/App.tsx
@@ -5,59 +5,75 @@
 
 import { getImageUrl } from "./util";
 
+const profiles = [
+{
+    name: 'Maria Skłodowska-Curie',
+    src: 'Maria',
+    size: {
+      width: 70,
+      height: 70
+    },
+    profession: 'physicist and chemist',
+    awards:{
+      cnt: 4,
+      description: ' (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)'
+    },
+    discovered: 'polonium (chemical element)'
+},
+{
+    name: 'Katsuko Saruhashi',
+    src: 'KatsukoSaruhashi',
+    size: {
+      width: 70,
+      height: 70
+    },
+    profession: 'geochemist',
+    awards:{
+      cnt: 2,
+      description: ' (Miyake Prize for geochemistry, Tanaka Prize)'
+    },
+    discovered: 'polonium (chemical element)'
+}
+];
+
+function Profile({name, src, width, height, profession, cnt, description, discovered}: {name : string, src : string, width : number, height : number, profession : string, cnt : number, description : string, discovered : string}){
+  return (
+    <>
+    <section className="profile">
+        <h2>{name}</h2>
+        <img
+          className="avatar"
+          src={getImageUrl(`${src}`)}
+          alt={name}
+          width={width}
+          height={height}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b> 
+            {profession}
+          </li>
+          <li>
+            <b>Awards: {cnt}</b> 
+            {description}
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {discovered}
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+}
+
 
 export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('Maria')}
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b> 
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (chemical element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src={getImageUrl('KatsukoSaruhashi')}
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b> 
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b> 
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+      <Profile name={profiles[0].name} src={profiles[0].src} width={profiles[0].size.width} height={profiles[0].size.height} profession={profiles[0].profession} cnt={profiles[0].awards.cnt} description={profiles[0].awards.description} discovered={profiles[0].discovered}/>
+       <Profile name={profiles[1].name} src={profiles[1].src} width={profiles[1].size.width} height={profiles[1].size.height} profession={profiles[1].profession} cnt={profiles[1].awards.cnt} description={profiles[1].awards.description} discovered={profiles[1].discovered}/>
     </div>
   );
 }
