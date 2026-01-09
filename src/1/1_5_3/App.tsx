4c1ac66bd9005dc28415b9a15fc713af81d59f70
DerEpic55 <dima20021234@gmail.com>
2026-01-10 00:06:25 +0600

    1.5.3

--- src/1/1_5_3/App.tsx
+++ src/1/1_5_3/App.tsx
@@ -3,11 +3,20 @@
   Извлеките компонент Card из приведенной ниже разметки и используйте параметр children для передачи ему различных JSX.
 */
 
+function Card({ children } : any) {
+  return(
+      <div className="card">
+        <div className="card-content">
+            {children}
+        </div>
+      </div>
+  );
+}
+
 export default function Profile() {
   return (
     <div>
-      <div className="card">
-        <div className="card-content">
+      <Card>
           <h1>Photo</h1>
           <img
             className="avatar"
@@ -16,14 +25,11 @@ export default function Profile() {
             width={70}
             height={70}
           />
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-content">
+      </Card>
+      <Card>
           <h1>About</h1>
           <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-        </div>
-      </div>
+      </Card>
     </div>
   );
 }
