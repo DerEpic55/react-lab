d33eb591fa840cd43c2c3ea32fb5f7118898609e
DerEpic55 <dima20021234@gmail.com>
2026-01-10 22:56:53 +0600

    3.1.1

--- src/3/3_1_1/App.tsx
+++ src/3/3_1_1/App.tsx
@@ -5,11 +5,28 @@
     Визуально вы должны увидеть, что щелчок на изображении удаляет фиолетовый фон и выделяет границу изображения. Щелчок за пределами изображения выделяет фон, но убирает выделение границы изображения.
 */
 
+import { useState } from "react";
+
 export default function Picture() {
+    const [isBackgroundActive, setIsBackgroundActive] = useState(true);
+    const [isPictureActive, setIsPictureActive] = useState(false);
+    
+    function handleClickBackground(){
+        setIsBackgroundActive(true);
+        setIsPictureActive(false);
+    }
+
+    function handleClickPicture(e : any){
+        e.stopPropagation();
+        setIsBackgroundActive(false);
+        setIsPictureActive(true);
+    }
+
     return (
-        <div className="background background--active">
+        <div className={`background${isBackgroundActive ? ' background--active' : ''}`} onClick={handleClickBackground}>
             <img
-                className="picture"
+                className={`picture${isPictureActive ? ' picture--active' : ''}`}
+                onClick={handleClickPicture}
                 alt="Rainbow houses in Kampung Pelangi, Indonesia"
                 src="/5qwVYb1.jpg"
             />
