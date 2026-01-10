4f64852d751c0a582c73cc194b9973ef78b2a46e
DerEpic55 <dima20021234@gmail.com>
2026-01-10 16:06:09 +0600

    2.5.2

--- src/2/2_5_2/processQueue.ts
+++ src/2/2_5_2/processQueue.ts
@@ -3,7 +3,14 @@ import { Que } from "./App";
 export function getFinalState(baseState: number, queue: Que[]) {
     let finalState = baseState;
 
-    // TODO: do something with the queue...
+    for(let el of queue){
+        if(typeof el === 'function'){
+            finalState = el(finalState);
+        }else{
+            finalState = el;
+        }
+    }
+
 
     return finalState;
 }
