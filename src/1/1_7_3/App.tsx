839408ff9eec694591f9b8faf6c0c1eb197fd333
DerEpic55 <dima20021234@gmail.com>
2026-01-10 14:54:52 +0600

    1.7.3

--- src/1/1_7_3/App.tsx
+++ src/1/1_7_3/App.tsx
@@ -5,23 +5,37 @@
 
 import { recipes } from './data';
 
+interface RecipeProps{
+    name : string;
+    ingredients : string[];
+}
+
+function Recipe({name, ingredients} : RecipeProps){
+    return(
+        <div>
+            <h2>{name}</h2>
+            <ul>
+                {ingredients.map(
+                    (ingredient) => (
+                        <li key={ingredient}>{ingredient}</li>
+                    )
+                )}
+            </ul>
+        </div>
+    );
+}
+
+
 export default function RecipeList() {
     return (
         <div>
-            <h1>Recipes</h1>
+             <h1>Recipes</h1>
             {recipes.map((recipe) => (
-                <div key={recipe.id}>
-                    <h2>{recipe.name}</h2>
-                    <ul>
-                        {recipe.ingredients.map(
-                            (ingredient) => (
-                                <li key={ingredient}>
-                                    {ingredient}
-                                </li>
-                            )
-                        )}
-                    </ul>
-                </div>
+                <Recipe 
+                    key={recipe.id} 
+                    name={recipe.name}
+                    ingredients={recipe.ingredients}
+                />
             ))}
         </div>
     );
