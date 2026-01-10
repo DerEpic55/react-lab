76b68a07c3587d4cec571e4cd8a930db7abcf686
DerEpic55 <dima20021234@gmail.com>
2026-01-10 14:45:31 +0600

    1.7.2

--- src/1/1_7_2/App.tsx
+++ src/1/1_7_2/App.tsx
@@ -9,6 +9,18 @@ export default function RecipeList() {
     return (
         <div>
             <h1>Recipes</h1>
+            {
+                recipes.map((recipe) => (
+                    <>
+                        <p>{recipe.name}</p>
+                        <ul>
+                            {recipe.ingredients.map((ingredient) => (
+                                <li>{ingredient}</li>   
+                            ))}
+                        </ul>
+                    </>
+                ))
+            };
         </div>
     );
 }
