bd59400495211e5e5263db9c214550c1f3a1effa
DerEpic55 <dima20021234@gmail.com>
2026-01-11 16:52:43 +0600

    4.2.2

--- src/4/4_2_2/App.tsx
+++ src/4/4_2_2/App.tsx
@@ -3,15 +3,22 @@
   Сделайте так, чтобы нажатие на кнопку "Поиск" наводило фокус на поле.
 */
 
+import { useRef } from "react";
+
 export default function Page() {
+  const ref = useRef(null);
+
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button onClick={() => {
+            ref.current.focus();
+        }}>Search</button>
       </nav>
-      <input
+      <input ref={ref}
         placeholder="Looking for something?"
       />
     </>
   );
 }
+
